de430f0370d8e9f0efbe246ed904e74fe92d70d1
kimcuc <kimcucduong0810@gmail.com>
2022-07-30 09:32:07 +0700

    first commit

--- asset/Random.js
+++ asset/Random.js
@@ -1,20 +1,6 @@
-document.getElementById('random').onclick = () => {
-    const valueRandom = Math.random();
-    const value=Math.floor(Math.random() *4 );
-    switch(value) {
-        case 0:
-        document.getElementsByTagName("h3") [0].innerText="Cúc";
-         break
-        case 1:
-        document.getElementsByTagName("h3") [0].innerText="Giang";
-        break
-        case 2:
-        document.getElementsByTagName("h3") [0].innerText="Giàu";
-        break
-        case 3:
-        document.getElementsByTagName("h3") [0].innerText="Hằng";
-        break
-    
-    }
+const name = ["Cúc", "Giang", "Giàu", "Hằng"];
 
+document.getElementById("random").onclick = () => {
+  const value = Math.floor(Math.random() * 4);
+  document.querySelector("h3").innerText = name[value];
 };
