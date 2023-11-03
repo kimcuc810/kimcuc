e8b111c4e13c587c25e223e99f934c9f54f3fe68
kimcuc <kimcucduong0810@gmail.com>
2023-11-03 14:55:25 +0700

    hello

--- asset/Random.js
+++
@@ -1,6 +0,0 @@
-const name = ["Cúc", "Giang", "Giàu", "Hằng"];
-
-document.getElementById("random").onclick = () => {
-  const value = Math.floor(Math.random() * 4);
-  document.querySelector("h3").innerText = name[value];
-};
